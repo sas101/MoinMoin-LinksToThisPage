cc77304a1219055b89a46429a21c45ee7795582b
Stefan Simroth <stefan.simroth@gmail.com>
2012-02-06 21:46:29 +0100

    use string substitution, fixes #4

--- LinksToThisPage.py
+++ LinksToThisPage.py
@@ -1,17 +1,18 @@
 """
-    LinksToThisPage action
+    MoinMoin Wiki - LinksToThisPage action
 
     Redirects to the full search using the linkto: syntax
     to find pages that link to the current page (a.k.a. "backlinks").
 
-    @copyright: 2009 Stefan Simroth
+    @copyright: 2009-2012 Stefan Simroth
 
-    @license: GNU GPL, to be found at http://www.gnu.org/licenses/gpl.html
+    @license: GNU GPLv3, to be found at http://www.gnu.org/licenses/gpl.html
 """
 
 from MoinMoin.Page import Page
 
 def execute(pagename, request):
-   url = Page(request, pagename).url(request, {'action': 'fullsearch', 'context': '180', 'value': 'linkto:"'+pagename+'"'})
+   linktothispage = 'linkto:"%s"' % pagename
+   url = Page(request, pagename).url(request, {'action': 'fullsearch', 'context': '180', 'value': linktothispage})
    request.http_redirect(url)
 
